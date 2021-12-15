4912623c03a3c249f08007db2617f1dc7522c03c
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-12-15 23:12:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 echo "downloading dependencies..."
 sleep 1
-sudo apt-get install python3 python3-pip curl
+sudo apt-get install python3 python3-pip curl ffmpeg
 clear
-sudo dnf install python3 python3-pip curl
+sudo dnf install python3 python3-pip curl ffmpeg
 clear
-sudo pacman -S python3 python3-pip curl
+sudo pacman -S python3 python3-pip curl ffmpeg
 clear
 echo "downloading python libs..."
 pip install -r requirements.txt
