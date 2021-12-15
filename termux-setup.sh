01b9e5d1cff0eb89ba7c938a2f43f29ccf612fdd
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-12-15 12:27:09 +0100

    Create termux-setup.sh

---
+++ termux-setup.sh
@@ -0,0 +1,21 @@
+echo "downloading dependencies..."
+
+sleep 1
+
+pkg install python curl
+
+
+
+
+
+
+
+
+
+echo "downloading python libs..."
+
+pip install -r requirements.txt
+
+clear
+
+echo "done!"
