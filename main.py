15fbb09130770a987a694f3a9aa81818b6b737f8
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-12-15 21:10:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ playlist = sys.argv[1]
 infos = os.popen(f'curl -s {playlist}').read()
 uh, mm = infos.split("Spotify.Entity = ")[1].split('M","ZW"]};')[0], 'M","ZW"]}'
 result = json.loads(f'{uh}{mm}')
-playlist_name = result['name']
+if os.popen('uname -o').read() == "Android":
+	playlist_name = f"/sdcard/PyDownify/{result['name']}"
+else:
+	playlist_name = result['name']
+os.system(f'mkdir "{playlist_name}"')
 def getUrl(name):
 	videosSearch = VideosSearch(name, limit = 1)
 	print(videosSearch.result()['result'][0]['title'], "...")
@@ -18,24 +22,20 @@ def download(name):
 	allstuff = getUrl(name)
 	url, title = allstuff['url'], allstuff['name'].replace('|', '')
 	YouTube(url).streams.filter(progressive=True, file_extension='mp4').first().download()
-	if os.popen("uname -o") == "Android":
-		os.system(f"mkdir sdcard/{playlist_name}/")
-		mp4_file, mp3_file = rf'{title}.mp4', rf'/sdcard/PyDownify/{playlist_name}/{title}.mp3'
-	else:
-		os.system(f"mkdir {playlist_name}")
-		mp4_file, mp3_file = rf'{title}.mp4', rf'{playlist_name}/{title}.mp3'
+	mp4_file, mp3_file = rf'{title}.mp4', rf'{playlist_name}/{title}.mp3'
 	audioclip = VideoFileClip(mp4_file).audio
 	audioclip.write_audiofile(mp3_file)
 	audioclip.close()
 	os.remove(f"{title}.mp4")
 for track in result['tracks']['items']:
+	title = VideosSearch(track['track']['name'], limit = 1).result()['result'][0]['title']
 	print(f"downloading ", end='')
 	try:
 		download(track['track']['name'])
 		print(track['track']['name'], ': {"statut": "downloaded succesfully"}')
 	except:
 		try:
-			os.system(f'rm {title}.mp4')
+			os.system(f'rm *.mp4')
 		except:
-			os.system(f'rm {title}.mp3')
+			os.system(f'rm "{playlist_name}/{title}.mp3"')
 		print(track['track']['name'], ': {"statut": "the download encountred some errors, please retry later"}')
