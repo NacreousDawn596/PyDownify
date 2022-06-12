e4b0c7c3a861aea57ab062f7bbc9ad790404f6a4
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2022-06-12 20:11:27 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-echo "downloading dependencies..."
-sleep 1
-sudo apt-get install python3 python3-pip curl wget ffmpeg -y
-clear
-echo "downloading libs"
-pip3 install youtube-dl==2021.12.17 youtube-search-python==1.6.5 bs4==0.0.1 requests argparse
-clear
-echo "done!"
-echo "now you can run it with => python3 main.py --help"
