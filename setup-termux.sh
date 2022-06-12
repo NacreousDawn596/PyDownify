fc41f4ce27cbd7f4d82adb59d53051aa99eb4067
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2022-06-12 20:11:15 +0100

    Delete setup-termux.sh

--- setup-termux.sh
+++
@@ -1,9 +0,0 @@
-echo "downloading dependencies..."
-sleep 1
-apt-get install python3 curl wget ffmpeg -y
-clear
-echo "downloading libs"
-pip3 install youtube-dl==2021.12.17 youtube-search-python==1.6.5 bs4==0.0.1 requests argparse
-clear
-echo "done!"
-echo "now you can run it with => python3 main.py --help"
