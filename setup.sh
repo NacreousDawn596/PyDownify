ee46fd0d8af5b972fa0b9e3b881a62675dfaefe9
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2022-06-12 17:38:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 echo "downloading dependencies..."
 sleep 1
-sudo apt-get install python2 python3 python3-pip curl wget ffmpeg -y
-clear
-echo "downloading python2 pip"
-python3 get-pip.py
+sudo apt-get install python3 python3-pip curl wget ffmpeg -y
 clear
 echo "downloading libs"
-pip2 install requests argparse
-pip3 install youtube-dl==2021.12.17 youtube-search-python==1.6.5 bs4==0.0.1
+pip3 install youtube-dl==2021.12.17 youtube-search-python==1.6.5 bs4==0.0.1 requests argparse
 clear
 echo "done!"
 echo "now you can run it with => python3 main.py --help"
