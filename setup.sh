dc76e2be45fc593f1c91196140fe3427f150e54c
NacreousDawn596 <kamalafriadwac@gmail.com>
2023-05-02 17:47:44 +0100

    fixes

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 echo "downloading dependencies..."
 sleep 1
-apt-get install python python-pip curl wget ffmpeg -y
+sudo apt-get install python python-pip curl wget ffmpeg -y
 clear
 echo "downloading libs"
-pip3 install yt-dlp==2021.12.17 youtube-search-python==1.6.5 bs4==0.0.1 requests
+pip3 install yt-dlp youtube-search-python==1.6.5 bs4==0.0.1 requests
 clear
 echo "done!"
 echo "now you can run it with => python3 main.py --help"
